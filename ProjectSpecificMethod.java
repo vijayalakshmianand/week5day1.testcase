995b48355ecc68c695e1b44bc411e43dca8329c4
vijayalakshmianand <149588076+vijayalakshmianand@users.noreply.github.com>
2023-11-20 22:24:42 +0800

    Add files via upload

---
+++ ProjectSpecificMethod.java
@@ -0,0 +1,35 @@
+package week5D1.testcase;
+
+import java.sql.Driver;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class ProjectSpecificMethod {
+	
+	public ChromeDriver driver;
+	
+	
+	@BeforeMethod
+	
+	public void preCondition() {
+		
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("DemoSalesManager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		
+	}
+	@AfterMethod
+public void postCondition() {
+	
+	driver.close();
+	
+	
+}
+}
